d4d91f1063396fd59a1573dbfc1717924f987277
Artur <65artur65@gmail.com>
2024-03-19 18:03:56 +0000

    fix:incorrect use of echo

--- element.sh
+++ element.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
-echo -c "\nPlease provide an element as an argument.\n"
+echo -e "\nPlease provide an element as an argument.\n"
 
 if [[ ! -z $1 ]]
 then
-  
-else
-  
+    echo "test"
 fi
