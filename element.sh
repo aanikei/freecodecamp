b1495449a1fab92f60ab5b6f4e2275d4c74dbabf
Artur <65artur65@gmail.com>
2024-03-19 18:35:06 +0000

    feat:correct output

--- element.sh
+++ element.sh
@@ -5,5 +5,7 @@ echo -e "\nPlease provide an element as an argument.\n"
 
 if [[ ! -z $1 ]]
 then
-    echo "test"
+  RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN elements USING(atomic_number) WHERE atomic_number = $1 OR symbol = '$1' OR name = '$1';")
+  read atomic_number BAR name BAR symbol BAR type BAR atomic_mass BAR melting_point_celsius BAR boiling_point_celsius <<< $(echo "$RESULT")
+  echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point_celsius celsius and a boiling point of $boiling_point_celsius celsius."
 fi
