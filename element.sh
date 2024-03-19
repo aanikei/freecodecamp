f813f1512990fed8f12b05e28e407c8d452cf00d
Artur <65artur65@gmail.com>
2024-03-19 17:57:28 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+echo -c "\nPlease provide an element as an argument.\n"
+
+if [[ ! -z $1 ]]
+then
+  
+else
+  
+fi
